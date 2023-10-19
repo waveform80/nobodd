5b1fb987b3a352733cf23336a180c20088a5d604
Dave Jones <dave.jones@canonical.com>
2023-10-20 00:34:50 +0100

    Fix the BootHandler implementations
    Works in manual tests now (extracting machine's specific boot partition
    file from within an unpacked image).

--- nobody/main.py
+++ nobody/main.py
@@ -11,26 +11,32 @@ from .tftpd import TFTPBaseHandler, TFTPBaseServer
 class BootHandler(TFTPBaseHandler):
     def resolve_path(self, filename):
         p = Path(filename)
-        if p.parts:
-            serial = p.parts[0]
-            boot_filename = Path.joinpath(*p.parts[1:])
+        if not p.parts:
+            raise FileNotFoundError()
+        serial = p.parts[0]
+        print(repr(serial))
+        boot_filename = Path('').joinpath(*p.parts[1:])
+        print(repr(boot_filename))
+        try:
+            fs = self.server.bootfs[serial]
+        except KeyError:
             try:
-                fs = self.server.bootfs[serial]
+                image = self.server.images[serial]
             except KeyError:
                 try:
-                    image = self.server.images[serial]
+                    (
+                        image_filename, partition, mac
+                    ) = self.server.machines[serial]
                 except KeyError:
-                    try:
-                        (
-                            image_filename, partition, mac
-                        ) = self.server.machines[serial]
-                    except KeyError:
-                        raise FileNotFoundError(filename)
-                    image = DiskImage(image_filename)
-                    self.server.images[serial] = image
-                fs = FatFileSystem(image.partitions[partition].data)
-                self.server.bootfs[serial] = fs
-            return fs.root / boot_filename
+                    raise FileNotFoundError(filename)
+                image = DiskImage(image_filename)
+                self.server.images[serial] = image
+                print(repr(image))
+                print(repr(image.partitions[partition]))
+            fs = FatFileSystem(image.partitions[partition].data)
+            self.server.bootfs[serial] = fs
+            print(repr(fs))
+        return fs.root / boot_filename
 
 
 class BootServer(ThreadingMixIn, TFTPBaseServer):
@@ -51,10 +57,10 @@ class BootServer(ThreadingMixIn, TFTPBaseServer):
 def main():
     machines = {
         '89025d75': (
-            'ubuntu-22.04.3-preinstalled-server-arm64+raspi.img', 1,
+            'ubuntu.img', 1,
             'dc:a6:32:02:36:ba'),
     }
-    with BootServer(('::', 69), machines) as server:
+    with BootServer(('::', 1069), machines) as server:
         server.logger.addHandler(logging.StreamHandler(sys.stderr))
-        server.logger.setLevel(logging.INFO)
+        server.logger.setLevel(logging.DEBUG)
         server.serve_forever()
