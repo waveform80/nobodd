651bb5f6e4a46be4010f65e03f1cf0c1518eb8a7
Dave Jones <dave.jones@canonical.com>
2024-03-02 11:02:04 +0000

    Update copyrights

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,8 @@
 #
 # nobodd: a boot configuration tool for the Raspberry Pi
 #
-# Copyright (c) 2023 Dave Jones <dave.jones@canonical.com>
-# Copyright (c) 2023 Canonical Ltd.
+# Copyright (c) 2023-2024 Dave Jones <dave.jones@canonical.com>
+# Copyright (c) 2023-2024 Canonical Ltd.
 #
 # SPDX-License-Identifier: GPL-3.0
 
--- docs/subst.rst
+++ docs/subst.rst
@@ -1,2 +1,9 @@
+.. nobodd: a boot configuration tool for the Raspberry Pi
+..
+.. Copyright (c) 2024 Dave Jones <dave.jones@canonical.com>
+.. Copyright (c) 2024 Canonical Ltd.
+..
+.. SPDX-License-Identifier: GPL-3.0
+
 .. |bug-link| replace::
    Please report bugs at: https://github.com/waveform80/nobodd/issues
